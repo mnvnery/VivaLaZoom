03e48fa77c9090f287a54272861b5e71d2b65b12
Maria Nery <67118326+mnvnery@users.noreply.github.com>
2022-09-14 10:55:17 +0100

    react player

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Image from 'next/image'
 import { request } from '../lib/datocms'
 import { motion } from "framer-motion"
+import dynamic from 'next/dynamic'
+
+const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 
 const ABOUT_QUERY = `{
   about {
@@ -23,6 +26,18 @@ export async function getStaticProps() {
 
 export default function Home({data}) {
   return (
+    <>
+    <div className="relative mt-10 mb-8 rounded-xl pt-[56.25%] md:mt-0 xxl:mt-10 xxl:mb-16">
+        <ReactPlayer
+          url='https://vimeo.com/747234731'
+          playing
+          muted
+          loop
+          width="100%"
+          height="100%"
+          className="absolute top-0 left-0 overflow-hidden rounded-xl"
+        />
+      </div>
     <motion.div initial={{ opacity: 0, y: 100 }}
     whileInView={{ opacity: 1, y: 0 }}
     transition={{ duration: 0.5 }}>
@@ -30,5 +45,6 @@ export default function Home({data}) {
         <Image src={data.about.image1.url} objectFit='cover' layout='fill'/>
       </div>
     </motion.div>
+    </>
   )
 }
